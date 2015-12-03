e6470b5d2ee21cc13c285a06cf6d1f27a1de3009
Eric Lindahl <eric@sciumo.com>
2015-12-02 22:46:38 -0500

    Convert CSV string boolean to bool

--- bin/cvtdict.js
+++ bin/cvtdict.js
@@ -6,6 +6,21 @@ var csvin = process.argv[2];
 var jsonout = process.argv[3];
 //end_parsed will be emitted once parsing finished
 converter.on("end_parsed", function (jsonArray) {
+   for( var idx = 0; idx < jsonArray.length; idx++ ){
+     var obj = jsonArray[idx];
+     if( obj ){
+       Object.keys(obj).forEach( function(key){
+         var val = obj[key];
+         if( typeof val === "string" ){
+           if( val.toUpperCase() === "TRUE" ){
+             obj[key] = true;
+           }else if( val.toUpperCase() === "FALSE"){
+             obj[key] = false;
+           }
+         }
+       });
+     }
+   }
    fs.writeFile(jsonout, JSON.stringify(jsonArray,null,2), 'utf-8');
 });
 
